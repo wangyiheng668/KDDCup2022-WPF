30148b764f0dcd2e46b1527a4510fd1e3c7c4564
wangyiheng668 <2495032682@qq.com>
2024-01-17 14:24:11 +0800

    测试

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -12,6 +12,7 @@ import joblib
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+###wangyiheng
 
 
 # ----------------------------NN 1 -----------------------------------
@@ -158,7 +159,7 @@ def forecast(settings):  # official required
 
 
 def get_lag_rolling_feature(data, roll_column, lags, period, id_col, agg_funs, feature_cols=[]):
-    # (lag, lag+period）
+    # (lag, lag+period＄1�7
     for lag in lags:
         for agg_fun in agg_funs:
             feature_col = roll_column + '_lag{}_roll{}_{}'.format(lag, period, agg_fun)
